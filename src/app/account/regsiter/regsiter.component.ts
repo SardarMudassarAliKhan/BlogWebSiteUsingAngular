5131b5f36440a4963b96cdddeb03f88bc01c2b88
mudassarali.official <mudassarali.official@gmail.com>
2024-02-28 04:27:03 +0500

    BWS-02-28-24 FC-Mvvm Architecture Implementation

---
+++ src/app/account/regsiter/regsiter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regsiter',
+  templateUrl: './regsiter.component.html',
+  styleUrl: './regsiter.component.css'
+})
+export class RegsiterComponent {
+
+}
